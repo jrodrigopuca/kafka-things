1f981539179196e0acafe6a0cd4caff6ab5fb81d
Juan Puca <japuca@teco.com.ar>
2023-02-10 17:47:30 -0300

    pipreqs

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ NodeJS
 - node consumer.js
 
 Python
+Solo es necesario instalar el kafka-python
 - python -m pip install kafka-python
 - python producer.py
 - python consumer.py
+
+generar requirements
+- python -m pip install pipreqs
+- python -m  pipreqs.pipreqs .
--- py/producer.py
+++ py/producer.py
@@ -5,6 +5,7 @@ from datetime import datetime
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 topic = 'strings'
 
+# send messages
 for i in range(100):
     messageToSend = f'msg:{i} en {datetime.now()}'
     producer.send(topic,messageToSend.encode('utf-8'))
